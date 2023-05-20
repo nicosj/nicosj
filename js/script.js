b6cc6557b4bc9c5b96aa06ff44551f99532518f2
Nicolas Rodriguez <nico.river09.9@gmail.com>
2023-05-20 19:00:08 -0300

    fix: menuuu

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var a=document.getElementById("pivot");
+a.addEventListener("click",function() {
+    // obtener el elemento y ageragr height 0px
+
+    var c = document.getElementById("nav-check");
+    c.checked = false;
+})
